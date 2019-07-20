617319e8e4525152270eb6970b5c37f09e23fec9
kawa557 <kawa557@gamil.com>
2019-07-20 16:51:04 +0900

    “fimfiifsidfjsfjs

---
+++ config/routes.rb
@@ -0,0 +1,28 @@
+Rails.application.routes.draw do
+
+  #get 'relationships/create'
+  #get 'relationships/destroy'
+  #get 'favorites/create'
+  #get 'favorites/destroy'
+  root 'tweets#index'
+
+  #get 'tweets/new'
+  #get 'tweets/index'
+  #get 'tweets/show'
+  #get 'tweets/create'
+  #get 'users/index'
+  #get 'users/show'
+  devise_for :users
+  resources :tweets do
+    resource :favorites, only: [:create, :destroy]
+  end
+
+  resources :users do
+    resource :relationships, only: [:create, :destroy]
+    get :follows, on: :member 
+    get :followers, on: :member
+  end
+
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
